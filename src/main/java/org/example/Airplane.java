c42ff2859e880898abb53185b91d4ea01902c71e
Morsko11 <morsko11@mail.ru>
2024-06-05 16:05:47 +0700

    Add StreamApi

---
+++ src/main/java/org/example/Airplane.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Airplane {
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString(){
+        return "name "+name;
+    }
+
+}
